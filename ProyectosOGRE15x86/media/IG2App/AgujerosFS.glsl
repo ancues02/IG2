93c10084191158a51afae61f5db12915000b1db4
andres <ancues02@ucm.es>
2020-12-09 12:42:59 +0100

    acabando p2.1
    falta pasar lo del VS al FS

--- ProyectosOGRE15x86/media/IG2App/AgujerosFS.glsl
+++ ProyectosOGRE15x86/media/IG2App/AgujerosFS.glsl
@@ -3,21 +3,19 @@
 uniform sampler2D texturaL; // tipo sampler2D para texturas 2D
 uniform vec4 InColor; 
 uniform vec4 OutColor;
+in vec3 vFrontColor; // color de la iluminación interpolado
+in vec3 vBackColor; // color de la iluminación interpolado
 in vec2 vUv0; // out del vertex shader
 out vec4 fFragColor; // out del fragment shader
 uniform float Flipping;
 
 void main() {
     bool frontFacing = (Flipping > -1)? gl_FrontFacing : ! gl_FrontFacing; 
-    vec3 color = vec3(texture(texturaL, vUv0)); // acceso a téxel
-
-    if (frontFacing) {
-        color*=vec3(OutColor.x,OutColor.y,OutColor.z);
-    }
-    else
-        color*=vec3(InColor.x,InColor.y,InColor.z);
-   
-    fFragColor = vec4(color, 1.0); // out
-     if (fFragColor.r >= 0.5)
-      discard; 
+    vec3 color = texture(texturaL, vUv0).rgb;
+    if (color.r >= 0.5)
+      discard;
+    if (frontFacing) color = vFrontColor  * OutColor.rgb;
+    else color = vBackColor  * InColor.rgb;
+    fFragColor = vec4(color, 1.0);
+    
 }
--- ProyectosOGRE15x86/media/IG2App/AgujerosVS.glsl
+++ ProyectosOGRE15x86/media/IG2App/AgujerosVS.glsl
@@ -1,13 +1,39 @@
 #version 330 core
 
 in vec4 vertex; // atributos de los vértices a procesar
+in vec3 normal;
 in vec2 uv0; // coordenadas de textura 0
+
+uniform mat4 modelViewMat; 
 uniform mat4 modelViewProjMat; // constante de programa
+uniform mat4 normalMat;
+
+uniform vec3 lightAmbient; 
+uniform vec3 lightDiffuse;
+uniform vec4 lightPosition;
+
+uniform vec3 materialDiffuse;
 // out vec4 gl_Position; // obligatoria predefinida
 out vec2 vUv0; // out del vertex shader
-
+out vec3 vFrontColor;
+out vec3 vBackColor;
  
-void main() {
-    vUv0 = uv0; // se pasan las coordenadas de textura
-    gl_Position = modelViewProjMat * vertex; //obligatorio
+float  diff(vec3 cVertex, vec3 cNormal){
+    vec3 lightDir=lightPosition.xyz;
+    if(lightPosition.w==1)
+        lightDir=lightPosition.xyz-cVertex;
+    return max(dot(cNormal,normalize(lightDir)),0.0);
+}
+
+void main(){
+    vec3 ambient = lightAmbient * materialDiffuse; 
+	vec3 viewVertex = vec3(modelViewMat * vertex);
+	vec3 viewNormal = normalize(vec3(normalMat * vec4(normal,0)));
+	vec3 diffuse = diff(viewVertex, viewNormal) * lightDiffuse * materialDiffuse;
+
+	vFrontColor = ambient + diffuse;
+    diffuse=diff(viewVertex,-viewNormal)*lightDiffuse* materialDiffuse;
+    vBackColor=ambient + diffuse;
+    vUv0=uv0;
+    gl_Position=modelViewProjMat * vertex;
 }
