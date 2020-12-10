3876451a12572f2cf4e05611bab1c7a26ba9b717
andres <ancues02@ucm.es>
2020-12-10 13:40:51 +0100

    acabada 2.1

--- ProyectosOGRE15x86/media/IG2App/AgujerosFS.glsl
+++ ProyectosOGRE15x86/media/IG2App/AgujerosFS.glsl
@@ -1,21 +1,49 @@
 #version 330 core
 
-uniform sampler2D texturaL; // tipo sampler2D para texturas 2D
+uniform sampler2D texturaCorrosion; // tipo sampler2D para texturas 2D
+uniform sampler2D texturaBumpy; // tipo sampler2D para texturas 2D
 uniform vec4 InColor; 
 uniform vec4 OutColor;
-in vec3 vFrontColor; // color de la iluminación interpolado
-in vec3 vBackColor; // color de la iluminación interpolado
+in vec3 vViewNormal;
+in vec3 vViewVertex;
 in vec2 vUv0; // out del vertex shader
 out vec4 fFragColor; // out del fragment shader
 uniform float Flipping;
 
+uniform vec3 lightAmbient; 
+uniform vec3 lightDiffuse;
+uniform vec4 lightPosition;
+
+uniform vec3 materialDiffuse;
+
+float  diff(vec3 cVertex, vec3 cNormal){
+    vec3 lightDir=lightPosition.xyz;
+    if(lightPosition.w==1)
+        lightDir=lightPosition.xyz-cVertex;
+    return max(dot(cNormal,normalize(lightDir)),0.0);
+}
+
+
 void main() {
     bool frontFacing = (Flipping > -1)? gl_FrontFacing : ! gl_FrontFacing; 
-    vec3 color = texture(texturaL, vUv0).rgb;
-    if (color.r >= 0.5)
+
+    vec3 ambient = lightAmbient * materialDiffuse; 
+    vec3 color = texture(texturaCorrosion, vUv0).rgb;
+    if (color.r >= 0.65)
       discard;
-    if (frontFacing) color = vFrontColor  * OutColor.rgb;
-    else color = vBackColor  * InColor.rgb;
+      
+    vec3 diffuse;
+    vec3 vColor;
+    if(frontFacing){//ponemos textura a la cara front y le aplicamos su color y sus luces
+        diffuse= diff(vViewVertex, vViewNormal) * lightDiffuse * materialDiffuse;
+        vColor = ambient + diffuse;
+        color = vColor * texture(texturaBumpy, vUv0).rgb * OutColor.rgb;
+    }
+    else{//aplicamos color y luces a la cara back, no le ponemos textura
+        diffuse=diff(vViewVertex,-vViewNormal)*lightDiffuse* materialDiffuse;
+        vColor = ambient + diffuse;
+        color = vColor  * InColor.rgb;
+    }    
     fFragColor = vec4(color, 1.0);
     
 }
--- ProyectosOGRE15x86/media/IG2App/AgujerosVS.glsl
+++ ProyectosOGRE15x86/media/IG2App/AgujerosVS.glsl
@@ -8,32 +8,17 @@ uniform mat4 modelViewMat;
 uniform mat4 modelViewProjMat; // constante de programa
 uniform mat4 normalMat;
 
-uniform vec3 lightAmbient; 
-uniform vec3 lightDiffuse;
-uniform vec4 lightPosition;
 
-uniform vec3 materialDiffuse;
 // out vec4 gl_Position; // obligatoria predefinida
 out vec2 vUv0; // out del vertex shader
-out vec3 vFrontColor;
-out vec3 vBackColor;
+out vec3 vViewNormal;
+out vec3 vViewVertex;
  
-float  diff(vec3 cVertex, vec3 cNormal){
-    vec3 lightDir=lightPosition.xyz;
-    if(lightPosition.w==1)
-        lightDir=lightPosition.xyz-cVertex;
-    return max(dot(cNormal,normalize(lightDir)),0.0);
-}
 
 void main(){
-    vec3 ambient = lightAmbient * materialDiffuse; 
-	vec3 viewVertex = vec3(modelViewMat * vertex);
-	vec3 viewNormal = normalize(vec3(normalMat * vec4(normal,0)));
-	vec3 diffuse = diff(viewVertex, viewNormal) * lightDiffuse * materialDiffuse;
-
-	vFrontColor = ambient + diffuse;
-    diffuse=diff(viewVertex,-viewNormal)*lightDiffuse* materialDiffuse;
-    vBackColor=ambient + diffuse;
+   
+    vViewVertex=vec3(modelViewMat * vertex);
+    vViewNormal = normalize(vec3(normalMat * vec4(normal,0)));
     vUv0=uv0;
     gl_Position=modelViewProjMat * vertex;
 }
