7fdd879e4ff786c055143f5067d641bede978e2e
ancues02 <ancues02@ucm.es>
2021-01-19 12:09:40 +0100

    añadida la iluminacion

--- ProyectosOGRE15x86/media/IG2App/Explota2GS.glsl
+++ ProyectosOGRE15x86/media/IG2App/Explota2GS.glsl
@@ -10,11 +10,11 @@ const float scale = 2;
 in vec2 vUv0[];
  //in vec2 vUv0;
  out vec2 _vUv0;
-//in vec3 vViewNormal;
-//in vec3 vViewVertex;
+in vec3 vViewNormal_[];
+in vec3 vViewVertex_[];
 
-//out vec3 vViewNormal;
-//out vec3 vViewVertex;
+out vec3 vViewNormal;
+out vec3 vViewVertex;
 
 vec3 normalVec(vec3 v[3]){
     vec3 normal= normalize(cross ((v[2]-v[1]),(v[0]-v[1])));
@@ -37,16 +37,20 @@ void main(){
        vec4(            0.0, 0.0,           0.0, 1.0)
    );
 
-    vec3 vertices[3] = vec3[](  gl_in[0].gl_Position.xyz * scale, 
-                                gl_in[1].gl_Position.xyz * scale,
-                                gl_in[2].gl_Position.xyz * scale);
+    vec3 vertices[3] = vec3[](  gl_in[0].gl_Position.xyz * tiempo, 
+                                gl_in[1].gl_Position.xyz * tiempo,
+                                gl_in[2].gl_Position.xyz * tiempo);
 
     vec3 dir = baricentroVec(vertices);
     for(int i = 0; i < 3; ++i){
         vec3 posDes = vertices[i] + dir * VD * tiempo;
         _vUv0=vUv0[i];
+        
         mat4 aux = (modelViewProjMat * yawMatrix);
+        vViewNormal=vViewNormal_[i];
+        vViewVertex=vViewVertex_[i];
         gl_Position = aux * vec4(posDes, 1.0);
+        
         EmitVertex();
     }
 
--- ProyectosOGRE15x86/media/IG2App/ExplotaGS.glsl
+++ ProyectosOGRE15x86/media/IG2App/ExplotaGS.glsl
@@ -8,18 +8,17 @@ in vec2 vUv0[];
 uniform float tiempo;
  //in vec2 vUv0;
  out vec2 _vUv0;
-//in vec3 vViewNormal;
-//in vec3 vViewVertex;
+in vec3 vViewNormal_[];
+in vec3 vViewVertex_[];
 
-//out vec3 vViewNormal;
-//out vec3 vViewVertex;
+out vec3 vViewNormal;
+out vec3 vViewVertex;
 vec3 normalVec(vec3 v[3]){
     vec3 normal= normalize(cross ((v[2]-v[1]),(v[0]-v[1])));
     return normal;
 }//vector normal al triangulo
 
 void main(){
-   
    
 
     vec3 vertices[3] = vec3[](  gl_in[0].gl_Position.xyz, 
@@ -30,6 +29,8 @@ void main(){
     for(int i = 0; i < 3; ++i){
         vec3 posDes = vertices[i] + dir * VD * tiempo;
          _vUv0=vUv0[i];
+         vViewNormal=vViewNormal_[i];
+         vViewVertex=vViewVertex_[i];
 /*gl_Position.x=gl_Position.x*cos(tiempo2pi)+sin(tiempo2pi)*gl_Position.z;
         gl_Position.z=gl_Position.x*-sin(tiempo2pi)+cos(tiempo2pi)*gl_Position.z;*/
         gl_Position = modelViewProjMat * vec4(posDes, 1.0);
