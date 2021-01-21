1a265f1d8831295639a9b661fa678984cd7d661d
Pablo Rodriguez Bobada <prodra02@ucm.es>
2021-01-21 16:21:37 +0100

    se ven las texturas

--- ProyectosOGRE15x86/media/IG2App/ExplotaVS.glsl
+++ ProyectosOGRE15x86/media/IG2App/ExplotaVS.glsl
@@ -1,7 +1,7 @@
 #version 330 core
 in vec4 vertex;
 in vec3 normal;
-in vec2 uV0;
+in vec2 uv0;
 
 out vec2 vUv0;
 out vec3 vViewNormal_;
@@ -14,6 +14,6 @@ uniform mat4 normalMat;
 void main(){
     vViewVertex_=vec3(modelViewMat * vertex);
     vViewNormal_ = normalize(vec3(normalMat * vec4(normal,0)));
-    vUv0=uV0;
+    vUv0=uv0;
     gl_Position = vertex;
 }
